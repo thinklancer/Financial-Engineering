6f83400541576c36673a2285b3fe42228993f660
thinklancer <thinklancer@gmail.com>
2013-04-01 17:37:01 +0200

    improve the code to make it appliable

--- hw5.py
+++ hw5.py
@@ -33,32 +33,34 @@ if __name__ == "__main__":
     # another way
     import bfc.BinomialTree as BT
     n = srl.n
+    expire = 5
     bt = BT.BinomialTree(n)
     qu = 0.5
     qd = 1-qu
+    fr = 0.045
     bt.setNode(0,0,1)
     for i in range(1,n):
         # j=0 case
         bt.setNode(i,0,bt.getNode(i-1,0)/2./(1+srl.getNode(i-1,0)))
         for j in range(1,i):
             bt.setNode(i,j,0.5*(bt.getNode(i-1,j-1)/(1+srl.getNode(i-1,j-1))+bt.getNode(i-1,j)/(1+srl.getNode(i-1,j))))
-            if i>4:
-                for j in range(i-2,i):
-                    bt.setNode(i,j,0)
         # j=i case
-        if i < 5:
-            bt.setNode(i,i,0.5*(bt.getNode(i-1,i-1)/(1+srl.getNode(i-1,i-1))))
+        bt.setNode(i,i,0.5*(bt.getNode(i-1,i-1)/(1+srl.getNode(i-1,i-1))))
+        # check option excised
+        if i == expire:
+            for j in range(i+1):
+                if srl.getNode(i,j) < fr:
+                    bt.setNode(i,j,0.)
     
-    
-    pay = BT.BinomialTree(11)
+    #pay = BT.BinomialTree(11)
     price = 0
-    for i in range(5,11):
-        s = [(srl.getNode(i,j)-0.045)/(1+srl.getNode(i,j))*bt.getNode(i,j) for j in range(i-2)]
-        for j in range(i-2):
-            pay.setNode(i,j,(srl.getNode(i,j)-0.045)/(1+srl.getNode(i,j)))
+    for i in range(expire,11):
+        s = [(srl.getNode(i,j)-fr)/(1+srl.getNode(i,j))*bt.getNode(i,j) for j in range(i+1)]
+        #for j in range(i+1):
+        #pay.setNode(i,j,(srl.getNode(i,j)-0.045)/(1+srl.getNode(i,j)))  #payment lattice
         price += sum(s)
     print price*1000000
-    with open('test.dot','w') as fi:
-        pay.showData(fi,form='{0:.2f}',coef=100)
+    #with open('test.dot','w') as fi:
+    #pay.showData(fi,form='{0:.2f}',coef=100)
     
 
